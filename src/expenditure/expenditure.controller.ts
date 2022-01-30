a14f787fb63cff8bfe6eed6add8594b7e67fdc88
danielenyata <daniel.kwame.okyere101@gmail.com>
2022-01-30 10:08:04 +0000

    Update expenditure.controller.ts

--- src/expenditure/expenditure.controller.ts
+++ src/expenditure/expenditure.controller.ts
@@ -6,17 +6,20 @@ import {
   Patch,
   Param,
   Delete,
-  Req
+  Req,
+  UseGuards
 } from "@nestjs/common";
 import { ExpenditureService } from "./expenditure.service";
 import { CreateExpenditureDto } from "./dto/create-expenditure.dto";
 import { UpdateExpenditureDto } from "./dto/update-expenditure.dto";
 import { CreateUserDto } from "src/user/dto/create-user.dto";
+import { JwtAuthGuard } from "src/auth/jwt-auth.gaurd";
 
 @Controller("expenditure")
 export class ExpenditureController {
   constructor(private readonly expenditureService: ExpenditureService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createExpenditureDto: CreateExpenditureDto, @Req() user:CreateUserDto) {
     const dto = {
@@ -26,16 +29,19 @@ export class ExpenditureController {
     return this.expenditureService.create(dto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   findAll() {
     return this.expenditureService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(":id")
   findOne(@Param("id") id: string) {
     return this.expenditureService.findOne(+id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(":id")
   update(
     @Param("id") id: string,
@@ -44,11 +50,13 @@ export class ExpenditureController {
     return this.expenditureService.update(+id, updateExpenditureDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(":id")
   remove(@Param("id") id: string) {
     return this.expenditureService.remove(+id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get("/user/:id")
   getUserExpenditures(@Param("id") id: string) {
     return this.expenditureService.getUserExpenditures(+id);
